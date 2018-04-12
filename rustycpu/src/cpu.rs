3df9ca39a48bf0fe4d2c3203b99373383c92d03a
tylerjaacks <GFT-f99-xH7-wmV>
2018-04-12 12:09:16 -0500

    Added CPU flag registers.

--- rustycpu/src/cpu.rs
+++ rustycpu/src/cpu.rs
@@ -1,9 +1,5 @@
-
+/* CPU object struct. */
 pub struct cpu {
-    // private static int instructionsToInterrupt;
-    // private static int instructionsSinceInterrupt = 0;
-    // private static boolean exitFlag = false;
-    // private static boolean kernelModeFlag = false;
     program_counter: i16,
     stack_pointer: i16,
     instruction_pointer: i16,
@@ -12,11 +8,17 @@ pub struct cpu {
     register2: i16,
     register3: i16,
     register4: i16,
+    sign_flag: bool,
+    zero_flag: bool,
+    parity_flag: bool,
+    carry_flag: bool,
+    auxiliary_carry: bool,
 }
 
+/* CPU implementation. */
 impl cpu {
     /* Constructs a new CPU struct with values. */
-    pub fn new(pc: i16, sp: i16, ip: i16, ac: i16, r1: i16, r2: i16, r3: i16, r4: i16) -> cpu {
+    pub fn new(pc: i16, sp: i16, ip: i16, ac: i16, r1: i16, r2: i16, r3: i16, r4: i16, sf: bool, zf: bool, pf: bool, cf: bool, acf: bool) -> cpu {
         cpu {
             program_counter: pc,
             stack_pointer: sp,
@@ -25,7 +27,12 @@ impl cpu {
             register1: r1,
             register2: r2,
             register3: r3,
-            register4: r4
+            register4: r4,
+            sign_flag: sf,
+            zero_flag: zf,
+            parity_flag: pf,
+            carry_flag: cf,
+            auxiliary_carry: acf,
         }
     }
 
@@ -39,22 +46,76 @@ impl cpu {
         self.register2 = 0;
         self.register3 = 0;
         self.register4 = 0;
+        self.sign_flag = false;
+        self.zero_flag = false;
+        self.parity_flag = false;
+        self.carry_flag = false;
+        self.auxiliary_carry = false;
     }
 
+    /* Increments the program counter. */
     pub fn increment_pc(&mut self) {
         self.program_counter += 1;
     }
 
+    /* Increments the stack pointer. */
     pub fn increment_sp(&mut self) {
         self.stack_pointer += 1;
     }
 
+    /* Increments the instruction pointer. */
     pub fn increment_ip(&mut self) {
         self.instruction_pointer += 1;
     }
 
+    /* Increments the accumulator. */
     pub fn increment_ac(&mut self) {
         self.accumulator += 1;
+    }
+
+    /* Sets the sign flag. */
+    pub fn set_sign_flag(&mut self) {
+        if (self.carry_flag == false) {
+            self.carry_flag = true;
+        } else {
+            self.carry_flag = false;
+        }
+    }
+
+    /* Sets the zero flag. */
+    pub fn set_zero_flag(&mut self) {
+        if (self.zero_flag == false) {
+            self.zero_flag = true;
+        } else {
+            self.zero_flag = false;
+        }
+    }
+
+    /* Sets the parity flag. */
+    pub fn set_parity_flag(&mut self) {
+        if (self.parity_flag == false) {
+            self.parity_flag = true;
+        } else {
+            self.parity_flag = false;
+        }
+    }
+
+    /* Sets the carry flag. */
+    pub fn set_carry_flag(&mut self) {
+        if (self.carry_flag == false) {
+            self.carry_flag = true;
+        } else {
+            self.carry_flag = false;
+        }
+    }
+
+    /* Sets the auxiliary carry flag. */
+    pub fn set_auxiliary_carry_flag(&mut self) {
+        if (self.auxiliary_carry == false) {
+            self.auxiliary_carry = true;
+        } else {
+            self.auxiliary_carry = false;
+        }
     }
 
     /* Prints the values of the CPU. */
@@ -66,6 +127,11 @@ impl cpu {
         println!("Register 1: {}", self.register1);
         println!("Register 2: {}", self.register2);
         println!("Register 3: {}", self.register3);
-        println!("Register 4: {}\n", self.register4);
+        println!("Register 4: {}", self.register4);
+        println!("Sign Flag: {}", self.sign_flag);
+        println!("Zero Flag: {}", self.zero_flag);
+        println!("Parity Flag: {}", self.parity_flag);
+        println!("Carry Flag: {}", self.carry_flag);
+        println!("Auxiliary Carry Flag: {}\n", self.auxiliary_carry);
     }
 }
--- rustycpu/src/main.rs
+++ rustycpu/src/main.rs
@@ -3,7 +3,7 @@ mod cpu;
 fn main() {
     println!("Welcome to RustyCPU!\n");
 
-    let mut rusty_cpu = cpu::cpu::new(1, 1, 1, 1, 1, 1, 1, 1);
+    let mut rusty_cpu = cpu::cpu::new(1, 1, 1, 1, 1, 1, 1, 1, true, true, true, true, true);
 
     /* Resets the cpu to default values. */
     rusty_cpu.reset_cpu();
@@ -15,4 +15,10 @@ fn main() {
         /* Prints the values of the cpu. */
         rusty_cpu.print_cpu();
     }
+
+    /* Resets the cpu to default values. */
+    rusty_cpu.reset_cpu();
+
+    /* Prints the values of the cpu. */
+    rusty_cpu.print_cpu();
 }
